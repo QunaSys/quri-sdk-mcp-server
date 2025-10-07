166d3fb9f8a313313e5671f812b3cfe2273def63
Daniel <chung@qunasys.com>
2025-10-08 01:36:19 +0800

    more resources

--- quri_sdk_mcp/bench_resource.py
+++ quri_sdk_mcp/bench_resource.py
@@ -92,4 +92,78 @@ _make_device_func_factory = TextResource(
 )
 
 
-all_bench_resources = (_qpe_eval, _make_device_func_factory)
+_hamiltonian_gen_api = '''
+def main(
+    atom: Atoms, 
+    basis: str = "sto-3g", 
+    spin: float = 0.0, 
+    charge: int = 0,
+    n_active_orb: int | None = None,
+    n_active_ele: int | None = None,
+) -> QubitHamiltonian:
+    """Generates molecular Hamiltonian.
+
+    Args:
+        atom: A string representing the coordinates of the atoms in a molecule.
+        basis: The basis set for generating the Hamiltonian. Default to sto-3g.
+        spin: The spin of the molecule. Follows the the standard convention of 
+            sz=0.5 for spin-1/2. Default to 0.0.
+        charge: Total charge of the molecule.
+        n_active_orb: Number of active orbitals.
+        n_active_ele: Number of active electrons.
+    """
+'''
+
+
+_mole_hamiltonian_gen = TextResource(
+    uri="bench://mole_hamiltonian",
+    name="molecular_hamiltonian_generation",
+    title="Device generation function factory from device specification",
+    description=f"""
+    Generate the input for the function "device_gen_func_factory"
+    """,
+    text=f"""
+    You are responsible for generating molecular hamiltonian.
+    Turn from user input into the input of {_hamiltonian_gen_api}. 
+    If the user provides no active space information, n_active_orb,
+    n_active_ele are both None. If user only provide n_active_orb, set
+    n_active_ele to be the same value.
+
+    {_common}
+    """,
+)
+
+
+_trotter_eval = '''
+def trotter_eval_main(
+    qubit_hamiltonian: QubitHamiltonian,
+    rs: Sequence[int] = (3, 5, 7, 9),
+    p: int = 2
+) -> float:
+    """Estimates the trotter error coefficient w.
+
+    Args:
+        qubit_hamiltonian: The qubit Hamiltonian.
+        rs: Number of Trotter steps for computing the Trotter error.
+        p: The Trotter order. Only accept p = 1 or 2.
+    """
+'''
+
+_trotter_eval_main = TextResource(
+    uri="bench://trotter_eval",
+    name="trotter_error_coefficient_eval",
+    title="trotter_error_coefficient_eval",
+    description=f"""
+    Evaluate the Trotter error coefficient w.
+    """,
+    text=f"""
+    You are responsible for evaluating the Trotter error coefficient.
+    Turn from user input into the input of {_trotter_eval}. 
+
+    {_common}
+    """,
+)
+
+all_bench_resources = (
+    _qpe_eval, _make_device_func_factory, _mole_hamiltonian_gen, _trotter_eval_main
+)
