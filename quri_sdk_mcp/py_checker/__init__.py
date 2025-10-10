f266372ef5dcca8f79d72ad281bd76be21523768
ThomasenQunasys <thomasen@qunasys.com>
2025-10-10 14:36:28 +0900

    fix last one

--- quri_sdk_mcp/py_checker/__init__.py
+++ quri_sdk_mcp/py_checker/__init__.py
@@ -1,3 +1,3 @@
-from py_checker.pyright_check import run_code_in_temporary_venv
+from quri_sdk_mcp.py_checker.pyright_check import run_code_in_temporary_venv
 
 __all__ = ["run_code_in_temporary_venv"]
